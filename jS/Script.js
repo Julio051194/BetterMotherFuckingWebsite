4fd852a7ffd95b0bc2b230fc70270547551d80f6
Julio051194 <46277148+Julio051194@users.noreply.github.com>
2019-01-04 13:52:09 +0100

    Update Script.js

--- jS/Script.js
+++ jS/Script.js
@@ -1,4 +1,4 @@
-window.setTimeout(tps, 2000);
+window.setTimeout(tps, 10000);
 function tps(){
 	if (confirm("Hey le site xxxvidsxxx est trop bien.Viens dessus stp please")){
 		window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
